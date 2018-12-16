acae3abc3a3d3f409a79c6e5adf6218eee4b32b0
毕夏 <978260577@qq.com>
2018-12-16 23:59:40 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,29 @@
+##汉诺塔
+#
+# def move(n,start,temp,target):
+#     if n == 1:
+#         print(start,'->',target)
+#     else:
+#         move(n-1,start,target,temp)
+#         print(start,'->',target)
+#         move(n-1,temp,start,target)
+#
+#
+# move(3,'A','B','C')
+# import re
+# x = 4
+# bx = bin(x)
+# bx = re.sub('0b','',bx)
+# bx = list(bx)
+# bx = [0] * 3 + bx
+# print(bx)
+# a = {'key': 10,'key': 11}
+# print(a)
+# a = dict(['a','b'],[1,2])
+# print(a)
+
+# a = [1,2,3,8,9,5,6,4]
+# b = 'abcd'
+# print(max(b))
+
+
