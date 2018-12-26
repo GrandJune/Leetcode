15baacaf6452fc537e189083541033a1e4224f09
毕夏 <978260577@qq.com>
2018-12-26 23:26:46 +0800

    递归算法；二叉树
    用来理解递归和二叉树

---
+++ LeetCode算法/617合并二叉树.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2018/12/26 23:25
+# @Author   : Junee
+# @FileName: 617合并二叉树.py
+# @Software  : PyCharm
+# Observing PEP 8 coding style
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+# 递归
+class Solution(object):
+    def mergeTrees(self, t1, t2):
+        """
+        :type t1: TreeNode
+        :type t2: TreeNode
+        :rtype: TreeNode
+        """
+        if t1 is None:
+            return t2
+        if t2 is None:
+            return t1
+        t1.val += t2.val
+        t1.left = self.mergeTrees(t1.left,t2.left)
+        t1.right = self.mergeTrees(t1.right,t2.right)
+        return t1
