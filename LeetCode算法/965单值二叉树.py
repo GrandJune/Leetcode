a4559bb794015fec3b060c0764ccc2396d9e580f
毕夏 <978260577@qq.com>
2019-01-09 19:20:38 +0800

    递归；迭代法的进一步联系

--- LeetCode算法/965单值二叉树.py
+++ LeetCode算法/965单值二叉树.py
@@ -4,6 +4,7 @@
 # @FileName: 965单值二叉树.py
 # @Software  : PyCharm
 # Observing PEP 8 coding style
+# 我的递归写法，比较冗长
 class Solution(object):
     def isUnivalTree(self, root):
         """
@@ -46,3 +47,27 @@ class Solution(object):
 
         dfs(root)
         return len(set(res)) == 1
+
+# While循环，迭代法
+class Solution(object):
+    def isUnivalTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if root == None:
+            return False
+        val = root.val
+        vl = [ root ]
+
+        while vl != [ ]:
+            node = vl.pop()
+            if val != node.val:
+                return False
+            else:
+                if node.right:
+                    vl.append(node.right)
+                if node.left:
+                    vl.append(node.left)
+                val = node.val
+        return True
